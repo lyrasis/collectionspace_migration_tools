be35cf6ca52cf1e4151ca981576d2cfc58891376
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-02-09 15:37:16 -0500

    add `CMT::Database::ExecuteQuery`

--- lib/collectionspace_migration_tools/database/execute_query.rb
+++ lib/collectionspace_migration_tools/database/execute_query.rb
@@ -9,7 +9,18 @@ module CollectionspaceMigrationTools
       class << self
         include Dry::Monads[:result]
 
-        def call(db:, sql:)
+        def call(query)
+          CMT::DB::OpenConnection.call.bind do |connection|
+            execute_query(connection.db, query).fmap do |result|
+              CMT::DB::CloseConnection.call(connection)
+              result
+            end
+          end
+        end
+
+        private
+        
+        def execute_query(db, sql)
           result = db.exec(sql)
         rescue StandardError => err
           Failure(CMT::Failure.new(context: "#{name}.#{__callee__}", message: err.message))
