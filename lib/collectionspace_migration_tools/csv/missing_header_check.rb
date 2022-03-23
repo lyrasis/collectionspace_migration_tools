fc0d0ab5f03d89eca1cf1bd0791a9fd812e3d160
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2022-03-23 19:04:31 -0400

    add `CSV::MissingHeaderCheck`

---
+++ lib/collectionspace_migration_tools/csv/missing_header_check.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'dry/monads'
+
+module CollectionspaceMigrationTools
+  module Csv
+    class MissingHeaderCheck
+      include Dry::Monads[:result]
+
+      class << self
+        def call(row)
+          self.new(row).call
+        end
+      end
+      
+      def initialize(row)
+        @row = row
+      end
+
+      def call
+        missing_headers = row.headers.select(&:blank?)
+        return Success(row) if missing_headers.empty?
+
+        Failure("#{missing_headers.length} field(s) lack a header value")
+      end
+      
+      private
+
+      attr_reader :row
+    end
+  end
+end
---
+++ spec/collectionspace_migration_tools/csv/missing_header_check_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require_relative '../../spec_helper'
+
+RSpec.describe CollectionspaceMigrationTools::Csv::MissingHeaderCheck do
+  let(:csv_path){ File.join(Bundler.root.to_s, 'spec', 'support', 'fixtures', 'csv', csv_name) }
+  let(:row){ CMT::Csv::FirstRowGetter.call(csv_path).value! }
+  
+  describe '#call' do
+    let(:result){ described_class.call(row) }
+
+    context 'when missing a header' do
+      let(:csv_name){ 'missing_header.csv' }
+      
+      it 'is Failure', :aggregate_failures do
+        expect(result).to be_a(Dry::Monads::Failure)
+        expect(result.failure).to eq('1 field(s) lack a header value')
+      end
+    end
+
+    context 'when all headers present' do
+      let(:csv_name){ 'unknown_header.csv' }
+      
+      it 'is Success', :aggregate_failures do
+        expect(result).to be_a(Dry::Monads::Success)
+        expect(result.value!).to be_a(CSV::Row)
+      end
+    end
+  end
+end
+
