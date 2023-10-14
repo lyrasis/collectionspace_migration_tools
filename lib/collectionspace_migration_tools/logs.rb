a5469cd4122c161265237e91a997984ee5060987
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2023-10-14 11:22:29 -0400

    Add AWS log timestamp/date converter methods

--- lib/collectionspace_migration_tools/logs.rb
+++ lib/collectionspace_migration_tools/logs.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "time"
+
 module CollectionspaceMigrationTools
   module Logs
     extend Dry::Monads[:result, :do]
@@ -16,6 +18,41 @@ module CollectionspaceMigrationTools
     else
       Success(response)
     end
+
+    # @param datestr [String] in format "2023-10-31", "2023-10-31_23:59",
+    #   "2023-10-31_23:59:06", or "2023-10-31_23:59:06.592"
+    # @return [Integer] epoch-with-milliseconds date/time expression used in
+    #   AWS log timestamps
+    def timestamp_from_datestring(datestr)
+      result = (Time.parse(datestr).to_f * 1000.0).to_i
+    rescue => err
+      msg = "#{err.message} IN #{err.backtrace[0]}"
+      Failure(
+        CMT::Failure.new(
+          context: "#{name}.#{__callee__}", message: msg
+        )
+      )
+    else
+      Success(result)
+    end
+
+    # @param timestamp [Integer] epoch-with-milliseconds date/time
+    #   expression used in AWS log timestamps
+    # @return [String] human-readable date/time in machine-local timezone
+    def datestring_from_timestamp(timestamp)
+      result = Time.at(0, timestamp, :millisecond)
+        .strftime("%Y-%m-%d_%H:%M:%S.%L")
+    rescue => err
+      msg = "#{err.message} IN #{err.backtrace[0]}"
+      Failure(
+        CMT::Failure.new(
+          context: "#{name}.#{__callee__}", message: msg
+        )
+      )
+    else
+      Success(result)
+    end
+
     def setup_client(client)
       if client.is_a?(Aws::CloudWatchLogs::Client)
         client
