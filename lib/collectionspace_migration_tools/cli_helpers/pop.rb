c9e81527bc7ad889c3b5695cb5299d5a60adb0f7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-03-07 15:08:52 -0500

    noblame: Whitespace and/or formatting changes only

--- lib/collectionspace_migration_tools/cli_helpers/pop.rb
+++ lib/collectionspace_migration_tools/cli_helpers/pop.rb
@@ -26,16 +26,24 @@ module CollectionspaceMigrationTools
       end
 
       def query_and_populate(rectypes, cache_type = nil)
+        starttime = Time.now
         rectypes.each do |rectype|
-          meth = cache_type.nil? ? :populate_both_caches : "populate_#{cache_type}_cache".to_sym
+          meth = if cache_type.nil?
+            :populate_both_caches
+          else
+            "populate_#{cache_type}_cache".to_sym
+          end
 
           rectype.send(meth).either(
             ->(success) { puts "Done" },
             ->(failure) {
-              puts "QUERY/POPULATE FAILED FOR #{rectype.to_s.upcase}\n#{failure}"
+              puts "QUERY/POPULATE FAILED FOR #{rectype.to_s.upcase}\n"\
+                "#{failure}"
             }
           )
         end
+        duration = Time.now - starttime
+        puts "Elapsed caching time: #{duration}"
       end
 
       def db_disconnect
