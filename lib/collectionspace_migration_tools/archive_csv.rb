e499b9fc5a3d19e488ed0d8d7580c42a35334d35
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2024-03-28 19:55:03 -0400

    Implement archive CSV Checker

--- lib/collectionspace_migration_tools/archive_csv.rb
+++ lib/collectionspace_migration_tools/archive_csv.rb
@@ -7,6 +7,8 @@ module CollectionspaceMigrationTools
   # completed ingest (with or without errors). Rows for batches deleted at an
   # earlier workflow stage are not added to the archive CSV.
   module ArchiveCsv
+    extend Dry::Monads[:result, :do]
+
     module_function
 
     # @return [String]
@@ -17,5 +19,27 @@ module CollectionspaceMigrationTools
 
     # @return [Boolean]
     def present? = File.exist?(path)
+
+    def file_check
+      case present?
+      when true
+        Success()
+      when false
+        Failure(file_check_failure_msg)
+      end
+    end
+
+    def parse
+      data = File.read(path)
+      table = CSV.parse(data, headers: true)
+    rescue => err
+      msg = "#{err.message} IN #{err.backtrace[0]}"
+      Failure(CMT::Failure.new(context: "#{self.class.name}.#{__callee__}",
+                               message: msg))
+    else
+      Success(table)
+    end
+
+    def file_check_failure_msg = "No archives CSV file present"
   end
 end
