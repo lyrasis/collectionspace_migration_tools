514c1362c7b60e56c4b0574672b50d2dbaeee4b8
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-04-22 12:50:36 -0400

    Add methods, test files, and tests for TermSource

--- lib/collectionspace_migration_tools/term_manager/term_source.rb
+++ lib/collectionspace_migration_tools/term_manager/term_source.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "roo"
+
 module CollectionspaceMigrationTools
   module TermManager
     class TermSource
@@ -8,7 +10,33 @@ module CollectionspaceMigrationTools
         @path = path
       end
 
-      def type = @type || get_type
+      def missing? = @missing ||= !File.exist?(path)
+
+      def type = @type ||= get_type
+
+      def workbook
+        return nil if missing?
+
+        @workbook ||= Roo::Excelx.new(path)
+      end
+
+      def sheet
+        return nil if missing?
+
+        @sheet ||= workbook.sheet("termData")
+      end
+
+      def rows
+        return nil if missing?
+
+        @rows ||= get_rows
+      end
+
+      def vocabs
+        return [] if missing?
+
+        @vocabs ||= build_vocabs
+      end
 
       private
 
@@ -20,6 +48,30 @@ module CollectionspaceMigrationTools
           .include?(path)
 
         :term_list
+      end
+
+      def get_rows
+        rows = sheet.parse(headers: true)
+        rows.shift
+        rows
+      end
+
+      def build_vocabs
+        return build_authority_vocabs if type == :authority
+
+        build_term_list_vocabs
+      end
+
+      def build_authority_vocabs
+        rows.group_by do |row|
+          "#{row["authorityType"]}/#{row["authoritySubtype"]}"
+        end
+          .map { |vocab, rows| AuthorityVocab.new(vocab, rows) }
+      end
+
+      def build_term_list_vocabs
+        rows.group_by { |row| row["term_list_shortIdentifier"] }
+          .map { |vocab, rows| TermListVocab.new(vocab, rows) }
       end
     end
   end
--- spec/collectionspace_migration_tools/term_manager/term_source_spec.rb
+++ spec/collectionspace_migration_tools/term_manager/term_source_spec.rb
@@ -3,14 +3,16 @@
 require_relative "../../spec_helper"
 
 RSpec.describe CollectionspaceMigrationTools::TermManager::TermSource do
-  subject(:source) do
-    CMT::TM::Project.new("napo").config
-    described_class.new(path)
-  end
+  subject(:source) { described_class.new(path) }
+
+  let(:bad_path) { "foo" }
+  let(:termlist_path) { File.join(fixtures_base, "shared_term_lists.xlsx") }
+  let(:authority_path) { File.join(fixtures_base, "shared_authorities.xlsx") }
 
   before(:all) do
     ENV["COLLECTIONSPACE_MIGRATION_TOOLS_SYSTEM_CONFIG"] =
       File.join(fixtures_base, "sys_config_w_term_manager.yml")
+    CMT.reset_config
   end
 
   after(:all) do
@@ -18,29 +20,112 @@ RSpec.describe CollectionspaceMigrationTools::TermManager::TermSource do
   end
 
   describe ".new" do
-    let(:path) { "foo" }
+    let(:path) { bad_path }
 
     it "returns TermSource" do
       expect(source).to be_a(CMT::TM::TermSource)
     end
   end
 
+  describe "#workbook" do
+    let(:result) { source.workbook }
+
+    context "with non-existent path" do
+      let(:path) { bad_path }
+
+      it "returns nil" do
+        expect(result).to be_nil
+      end
+    end
+
+    context "with good path" do
+      let(:path) { termlist_path }
+
+      it "returns Roo::Excelx" do
+        expect(result).to be_a(Roo::Excelx)
+      end
+    end
+  end
+
   describe "#type" do
-    let(:result) { source.type }
+    let(:result) do
+      CMT::TM::Project.new("napo").config
+      source.type
+    end
 
     context "when term list source" do
-      let(:path) { CMT.config.term_manager.term_list_sources.first }
+      let(:path) { puts CMT.config.term_manager.term_list_sources.first }
 
       it "returns as expected" do
         expect(result).to eq(:term_list)
       end
     end
 
-    context "when term list source" do
+    context "when authority source" do
       let(:path) { CMT.config.term_manager.authority_sources.first }
 
       it "returns as expected" do
         expect(result).to eq(:authority)
+      end
+    end
+  end
+
+  describe "#sheet" do
+    before(:each) do
+      CMT::TM::Project.new("napo").config
+      CMT.config.term_manager.term_list_sources << path
+    end
+    let(:path) { termlist_path }
+    let(:result) { source.sheet }
+
+    it "returns as expected" do
+      expect(result).to be_a(Roo::Excelx)
+    end
+  end
+
+  describe "#rows" do
+    let(:path) { termlist_path }
+
+    let(:result) do
+      CMT::TM::Project.new("napo").config
+      CMT.config.term_manager.term_list_sources << path
+      described_class.new(path).rows
+    end
+
+    it "returns as expected" do
+      expect(result).to be_a(Array)
+      expect(result.first).to be_a(Hash)
+    end
+  end
+
+  describe "#vocabs" do
+    let(:result) { described_class.new(path).vocabs }
+
+    context "when authority source" do
+      before(:each) do
+        CMT::TM::Project.new("napo").config
+        CMT.config.term_manager.authority_sources << path
+      end
+
+      let(:path) { authority_path }
+
+      it "returns as expected" do
+        expect(result).to be_a(Array)
+        expect(result.first).to be_a(CMT::TM::AuthorityVocab)
+      end
+    end
+
+    context "when termlist source" do
+      before(:each) do
+        CMT::TM::Project.new("napo").config
+        CMT.config.term_manager.term_list_sources << path
+      end
+
+      let(:path) { termlist_path }
+
+      it "returns as expected" do
+        expect(result).to be_a(Array)
+        expect(result.first).to be_a(CMT::TM::TermListVocab)
       end
     end
   end
