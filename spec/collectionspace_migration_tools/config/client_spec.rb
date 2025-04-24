f947651570f928a496b32199c5c0c14288971d60
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-04-24 18:44:40 -0400

    Auto populate site details in hosted client configs

---
+++ spec/collectionspace_migration_tools/config/client_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require_relative "../../spec_helper"
+
+RSpec.describe CollectionspaceMigrationTools::Config::Client do
+  let(:result) do
+    described_class.call(hash: config_hash)
+  end
+
+  context "when valid config" do
+    let(:config_hash) { valid_config_hash[:client] }
+
+    it "returns Success" do
+      expect(result).to be_a(Dry::Monads::Success)
+    end
+  end
+
+  context "when valid hosted client config" do
+    let(:config_hash) do
+      path = File.join(fixtures_base, "hosted_client_config_valid.yml")
+      CMT::Parse::YamlConfig.call(path).value![:client]
+    end
+
+    it "returns Success" do
+      mock_tenant = double("Tenant",
+        services_url: "https://ohiohistory.collectionspace.org/cspace-services/",
+        user_name: "admin@collectionspace.org",
+        admin_password: "abcdefgh")
+      allow(CHIA).to receive(:tenant_for) { mock_tenant }
+      expect(result).to be_a(Dry::Monads::Success)
+    end
+  end
+end
