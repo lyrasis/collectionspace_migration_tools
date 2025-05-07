e6dfcaff6874571eb0cdac9b2399bf7215c0b274
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-05-07 13:01:50 -0400

    Add #client method to Instance

--- lib/collectionspace_migration_tools/term_manager/instance.rb
+++ lib/collectionspace_migration_tools/term_manager/instance.rb
@@ -3,6 +3,8 @@
 module CollectionspaceMigrationTools
   module TermManager
     class Instance
+      attr_reader :id
+
       # @param id [String]
       # @param config [Hash]
       def initialize(id, config)
@@ -10,9 +12,26 @@ module CollectionspaceMigrationTools
         @config = config
       end
 
+      def client = @client ||= build_client
+
       private
 
       attr_reader :config
+
+      def build_client
+        setup_config
+        CollectionSpace::Client.new(
+          CollectionSpace::Configuration.new(**config)
+        )
+      end
+
+      def setup_config
+        if config.empty?
+          @config = CHIA.client_config_for(id)
+        elsif config.keys.length < 3
+          @config = CHIA.client_config_for(id).merge(config)
+        end
+      end
     end
   end
 end
