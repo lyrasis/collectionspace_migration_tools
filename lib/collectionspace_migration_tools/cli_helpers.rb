3730d7cf2070c73a50ec99774275a9e0c53d32e7
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-06-09 16:43:16 -0400

    refactor: Extract general CLI helpers from pop helpers

---
+++ lib/collectionspace_migration_tools/cli_helpers.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module CollectionspaceMigrationTools
+  module CliHelpers
+    ::CMT::CliHelpers = CollectionspaceMigrationTools::CliHelpers
+
+    module_function
+
+    def db_disconnect
+      CMT.connection.close
+      CMT.tunnel.close
+    end
+
+    def safe_db
+      yield
+    rescue => err
+      raise err if options[:debug]
+      warn err.message
+      db_disconnect
+      exit(1)
+    else
+      db_disconnect
+      exit(0)
+    end
+  end
+end
--- lib/collectionspace_migration_tools/cli_helpers/pop.rb
+++ lib/collectionspace_migration_tools/cli_helpers/pop.rb
@@ -29,23 +29,6 @@ module CollectionspaceMigrationTools
         duration = Time.now - starttime
         puts "Elapsed caching time: #{duration}"
       end
-
-      def db_disconnect
-        CMT.connection.close
-        CMT.tunnel.close
-      end
-
-      def safe_db
-        yield
-      rescue => err
-        raise err if options[:debug]
-        warn err.message
-        db_disconnect
-        exit(1)
-      else
-        db_disconnect
-        exit(0)
-      end
     end
   end
 end
