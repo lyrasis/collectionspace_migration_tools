d61439122de1a869b3d4beae1ae357874622ce4d
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-08-13 12:24:23 -0400

    feat: Proposed changes to harvester class

--- lib/collectionspace_migration_tools/harvester/harvester.rb
+++ lib/collectionspace_migration_tools/harvester/harvester.rb
@@ -1,19 +1,12 @@
 # frozen_string_literal: true
 
-require "forwardable"
-
-class CMT::Entity::Procedure
-  extend Forwardable
-  def_delegator :mapper, :service_path
-end
-
 module CollectionspaceMigrationTools
     module Harvester
       # Harvest XML from a CollectionSpace instance
       class Harvester
         include Dry::Monads[:result]
         include Dry::Monads::Do.for(:call, :harvest)
-  
+
         class << self
           def call(rec_type:)
             new(rec_type: rec_type).call
@@ -21,40 +14,26 @@ module CollectionspaceMigrationTools
         end
 
         def initialize(rec_type:)
-          @client = CMT::Client.call.value!
           @rec_type = rec_type
+          @target_dir = File.join(CMT.config.client.base_dir, "xml", rec_type)
         end
-  
+
         def call
-          _entity = yield convert_to_entity
-          _result = yield harvest
-  
-          Success()
+          @client = yield CMT::Client.call
+          @entity = yield CMT::RecordTypes.to_obj(rec_type)
+          @service_path = entity.service_path_full
+
+          result = yield harvest
+
+          Success(result)
         end
 
         private
 
-        attr_reader :client, :rec_type
-
-        def convert_to_entity
-          result = CMT::RecordTypes.to_obj(rec_type)
-          if result.success?
-            @entity = result.value!
-            Success(@entity)
-          else
-            Failure(CMT::Failure.new(
-              context: "#{self.class.name}.#{__callee__}",
-              message: "Failed to convert #{rec_type} to an entity: #{result.failure}"
-            ))
-          end
-        end
-
-        def entity
-          @entity ||= convert_to_entity.value!
-        end
+        attr_reader :client, :rec_type, :entity, :service_path, :target_dir
 
         def get_record(uri)
-          result = client.get uri
+          result = client.get(uri)
           Success(result)
         rescue CollectionSpace::RequestError => err
           msg = "Request failure getting record #{uri}: #{err.message}"
@@ -84,7 +63,7 @@ module CollectionspaceMigrationTools
         end
 
         def harvest
-          FileUtils.mkdir_p(File.join(CMT.config.client.base_dir, rec_type))
+          FileUtils.mkdir_p(target_dir)
 
           total_results = yield get_record_count
           harvested = 0
@@ -102,26 +81,12 @@ module CollectionspaceMigrationTools
             yield write_xml(record, result)
           end
 
-          puts "Harvested #{total_results} records to #{File.join(CMT.config.client.base_dir, rec_type)}"
-
-          return Success()
-        end
-
-        def service_path
-          @service_path ||= begin
-            if entity.respond_to?(:mapper) && entity.mapper.respond_to?(:service_path)
-              entity.mapper.service_path
-            elsif entity.respond_to?(:service_path)
-              entity.service_path
-            else
-              entity.name
-            end
-          end
+          return Success("Harvested #{total_results} records to #{target_dir}")
         end
 
         def write_xml(record, result)
           csid = record["uri"].split("/").last
-          filename = File.join(CMT.config.client.base_dir, "#{rec_type}/#{csid}.xml")
+          filename = File.join(target_dir, "#{csid}.xml")
 
           File.write(filename, result.xml.to_s)
           Success()
@@ -134,4 +99,3 @@ module CollectionspaceMigrationTools
       end
     end
   end
-  
