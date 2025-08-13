41064ca9fff8199dd4d464756e31a796d8ae350c
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-08-12 20:42:55 -0400

    test: Add tests for planned changes/refactoring where missing

--- spec/collectionspace_migration_tools/entity/authority_spec.rb
+++ spec/collectionspace_migration_tools/entity/authority_spec.rb
@@ -3,18 +3,21 @@
 require_relative "../../spec_helper"
 
 RSpec.describe CollectionspaceMigrationTools::Entity::Authority do
-  describe ".from_str" do
-    let(:result) { described_class.from_str(str) }
+  subject(:auth) { described_class.from_str(str) }
 
+  describe ".from_str" do
     context "valid rectype with slash" do
       let(:str) { "person/local" }
 
       it "constructs as expected" do
-        expect(result).to be_a(described_class)
-        expect(result.type).to eq("person")
-        expect(result.subtype).to eq("local")
-        expect(result.status.success?).to be true
-        expect(result.service_path).to eq("personauthorities")
+        expect(auth).to be_a(described_class)
+        expect(auth.type).to eq("person")
+        expect(auth.subtype).to eq("local")
+        expect(auth.status.success?).to be true
+        expect(auth.service_path).to eq("personauthorities")
+        expect(auth.service_path_full).to eq(
+          "personauthorities/urn:cspace:name(person)/items"
+        )
       end
     end
 
@@ -22,10 +25,10 @@ RSpec.describe CollectionspaceMigrationTools::Entity::Authority do
       let(:str) { "person-local" }
 
       it "constructs as expected" do
-        expect(result).to be_a(described_class)
-        expect(result.type).to eq("person")
-        expect(result.subtype).to eq("local")
-        expect(result.status.success?).to be true
+        expect(auth).to be_a(described_class)
+        expect(auth.type).to eq("person")
+        expect(auth.subtype).to eq("local")
+        expect(auth.status.success?).to be true
       end
     end
 
@@ -33,11 +36,20 @@ RSpec.describe CollectionspaceMigrationTools::Entity::Authority do
       let(:str) { "person" }
 
       it "constructs as expected" do
-        expect(result).to be_a(described_class)
-        expect(result.type).to eq("person")
-        expect(result.subtype).to eq("")
-        expect(result.status.failure?).to be true
+        expect(auth).to be_a(described_class)
+        expect(auth.type).to eq("person")
+        expect(auth.subtype).to eq("")
+        expect(auth.status.failure?).to be true
       end
+    end
+  end
+
+  describe "#cacheable_data_query" do
+    let(:str) { "person/local" }
+    let(:result) { auth.cacheable_data_query }
+
+    it "returns as expected" do
+      expect(result.value!).to match(/select 'personauthorities' as type,/)
     end
   end
 end
