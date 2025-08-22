69addb9ef7381dd53d405acafef3ee03a393130e
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-08-21 22:03:53 -0400

    feat: Extract the extraction of db credentials to CMT::Database

--- lib/collectionspace_migration_tools/database.rb
+++ lib/collectionspace_migration_tools/database.rb
@@ -7,6 +7,15 @@ module CollectionspaceMigrationTools
 
     module_function
 
+    # @param tenant [CHIA::Tenant, String]
+    # @return [String]
+    def db_credentials_for(tenant)
+      return extract_db_credentials(tenant) if tenant.respond_to?(:db_host)
+
+      t = CHIA.tenant_for(tenant)
+      extract_db_credentials(t)
+    end
+
     # @param tenant_name [nil, String]
     # @return [String]
     def tunnel_command(tenant_name = nil)
@@ -17,6 +26,16 @@ module CollectionspaceMigrationTools
         CMT.config.system.bastion_user,
         CMT.config.system.db_port
       )
+    end
+
+    # @param tenant [CHIA::Tenant]
+    def extract_db_credentials(tenant)
+      {
+        db_host: tenant.db_host,
+        db_username: tenant.db_user_name,
+        db_password: tenant.db_password,
+        db_name: tenant.db_name
+      }
     end
   end
 end
