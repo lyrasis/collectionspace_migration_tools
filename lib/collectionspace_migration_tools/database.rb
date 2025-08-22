a5673c398c48d8b588be06d578e9732d4dca8cd0
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-08-21 20:29:19 -0400

    refactor: Extract :tunnel_command method

--- lib/collectionspace_migration_tools/database.rb
+++ lib/collectionspace_migration_tools/database.rb
@@ -4,5 +4,16 @@ module CollectionspaceMigrationTools
   # Namespace shared data holder for database connection
   module Database
     ::CMT::DB = CollectionspaceMigrationTools::Database
+
+    module_function
+
+    def tunnel_command
+      tenant = CHIA.tenant_for(CMT.config.client.tenant_name)
+
+      tenant.db_tunnel_command(
+        CMT.config.system.bastion_user,
+        CMT.config.system.db_port
+      )
+    end
   end
 end
