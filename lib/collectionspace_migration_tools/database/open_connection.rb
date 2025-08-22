5dda3e04adfdae73c9e142bddb0fca5e0dacd476
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2025-08-22 16:01:17 -0400

    feat: Create connection for given or configured tenant

--- lib/collectionspace_migration_tools/database/open_connection.rb
+++ lib/collectionspace_migration_tools/database/open_connection.rb
@@ -15,8 +15,9 @@ module CollectionspaceMigrationTools
 
       def close
         if open?
+          dbname = db
           finish
-          puts "Closed DB connection"
+          puts "Closed DB connection to #{dbname}"
         else
           puts "DB connection already closed"
         end
@@ -26,25 +27,42 @@ module CollectionspaceMigrationTools
 
     # opens database connection through tunnel
     class OpenConnection
-      class << self
-        include Dry::Monads[:result]
+      extend Dry::Monads[:result, :do]
 
-        def call
+      class << self
+        # @param tenant_name [nil, String]
+        def call(tenant_name = nil)
+          tenant_name || CMT.config.client.tenant_name
+          creds = get_db_info(tenant_name)
           check_connection = CMT.connection
 
-          if check_connection&.open?
-            puts "DB connection already open. Using existing."
-            Success(check_connection)
-          else
-            CMT::DB::OpenTunnel.call.bind do
-              get_connection
-            end
+          return new_connection(tenant_name, creds) unless check_connection
+
+          unless check_connection&.open? &&
+              check_connection.db == creds[:dbname]
+            check_connection.close
+            return new_connection(tenant_name, creds)
           end
+
+          Success(check_connection)
         end
 
         private
 
-        def db_info
+        def new_connection(tenant_name, creds)
+          _tunnel = yield CMT::DB::OpenTunnel.call(tenant_name)
+          result = yield get_connection(creds)
+
+          Success(result)
+        end
+
+        def get_db_info(tenant_name)
+          return db_info_for_configured_tenant unless tenant_name
+
+          db_info_for_given_tenant(tenant_name)
+        end
+
+        def db_info_for_configured_tenant
           {
             host: CMT.config.system.db_connect_host,
             port: CMT.config.system.db_port,
@@ -54,15 +72,27 @@ module CollectionspaceMigrationTools
           }
         end
 
-        def get_connection
+        def db_info_for_given_tenant(tenant_name)
+          creds = CMT::Database.db_credentials_for(tenant_name)
+
+          {
+            host: CMT.config.system.db_connect_host,
+            port: CMT.config.system.db_port,
+            dbname: creds[:db_name],
+            user: creds[:db_username],
+            password: creds[:db_password]
+          }
+        end
+
+        def get_connection(creds)
           sleep(3)
-          connection = PG::Connection.new(**db_info)
+          connection = PG::Connection.new(**creds)
+          CMT.connection = connection
+          puts "New DB connection created for #{connection.db}"
+          Success(connection)
         rescue => err
           Failure(CMT::Failure.new(context: "#{name}.#{__callee__}",
             message: err.message))
-        else
-          CMT.connection = connection
-          Success(connection)
         end
       end
     end
