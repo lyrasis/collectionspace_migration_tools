22b3f03eef29abde508b79b4e9ccc6af7468d8af
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-02-25 14:25:57 -0500

    fix: Non-verbose config show command not finding config

--- lib/collectionspace_migration_tools/config.rb
+++ lib/collectionspace_migration_tools/config.rb
@@ -13,6 +13,7 @@ module CollectionspaceMigrationTools
     }
 
     # @param [:system, :redis, :term_manager]
+    # @return [String]
     def file_path(type)
       envkey = "COLLECTIONSPACE_MIGRATION_TOOLS_#{type.upcase}_CONFIG"
       envpath = ENV[envkey]
@@ -24,6 +25,27 @@ module CollectionspaceMigrationTools
       return dotfile if File.exist?(dotfile)
 
       File.join(Bundler.root, name)
+    end
+
+    # @param config [CMT::Configuration, NilClass>
+    # @return [Dry::Monads::Success<String>]
+    def current_client_config_name(config = nil)
+      system = if config
+        config.system
+      else
+        yield CMT::Config::System.call
+      end
+      path = system.config_name_file
+      content = File.read(path)
+      if content.empty?
+        return Failure(CMT::Failure.new(
+          context: "#{self.class.name}.#{__callee__}",
+          message: "No client config found. Try doing:\n"\
+          "  thor config switch {yourconfigname}. "
+        ))
+      end
+
+      Success(content)
     end
   end
 end
