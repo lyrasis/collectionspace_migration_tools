335f600a83ac98471f4e26ba9abdb8a86f2174af
Kristina Spurgin <kristina.spurgin@lyrasis.org>
2026-07-09 18:31:16 -0400

    fix: Revert attempt to override S3 token expiration

--- lib/collectionspace_migration_tools/build/s3_client.rb
+++ lib/collectionspace_migration_tools/build/s3_client.rb
@@ -36,10 +36,7 @@ module CollectionspaceMigrationTools
       attr_reader :profile, :bucket
 
       def create_client
-        client = Aws::S3::Client.new(
-          profile: profile
-        )
-        client.config.credentials.assume_role_params[:duration_seconds] = 21600
+        client = Aws::S3::Client.new(profile: profile)
       rescue => err
         msg = "#{err.message} IN #{err.backtrace[0]}"
         Failure(CMT::Failure.new(context: "#{self.class.name}.#{__callee__}",
